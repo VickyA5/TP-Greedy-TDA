168ba5bbe6e92c1ac1dc235f9fe65c7c3c1e8f5f
VickyA5 <vavalos@fi.uba.ar>
2024-09-26 18:04:11 -0300

    ahora se ejecuta pasando la ruta del archivo

--- README.md
+++ README.md
@@ -12,8 +12,18 @@ Trabajo Práctico N°1 de la materia Teoría de Algoritmos del curso Buchwald-Ge
 ## Instrucciones de uso
 
 
-Desde la carpeta base del trabajo ejecutar el comando:
+Desde la carpeta base del trabajo ejecutar el siguiente comando:
 
-`python3 problema.py`
+`python3 problema.py <ruta_archivo_txt>`
 
-Los resultados de las acciones individuales de cada jugador se guardarán en la carpeta outputs.
+Dentro de la carpeta `tests_cases` se encuentran, separados por carpetas, tanto los casos proporcionados por la cátedra como tests propios. 
+
+Para correr el programa con el set que se desee, se debe ingresar la ruta del archivo desde la carpeta base. Por ejemplo: 
+
+`python3 problema.py tests_cases/catedra/25.txt`
+
+El usuario puede agregar un archivo con su propio set de monedas en la carpeta de tests_cases y ejecutarlo de la siguiente forma:
+
+`python3 problema.py tests_cases/<nombre_archivo.txt>`
+
+Los resultados de las acciones individuales de cada jugador se guardarán en la carpeta `outputs`.
--- problema.py
+++ problema.py
@@ -8,6 +8,7 @@
 # La función debe devolver dos listas, la primera con los valores de las monedas que elige Sofia y la segunda con los valores de las monedas que elige Mateo.
 from collections import deque
 import os
+import sys
 
 
 def problema_monedas(fila):
@@ -44,59 +45,34 @@ def problema_monedas(fila):
     return sophia, mateo, esperados
 
 
-def tests():
+def tests(file_path):
     output_folder = "outputs"
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Tests de la cátedra
-    catedra_folder = "tests_cases/catedra"
-    test_files = ["20", "25", "50", "100", "1000", "10000", "20000"]
-    for i in range(0, len(test_files)):
-        with open(f"{catedra_folder}/{test_files[i]}.txt", "r") as file:
-            file.readline()
-            fila = list(int(i) for i in file.readline().split(";"))
-            sophia, mateo, esperados = problema_monedas(fila)
-            ganancia_sophia = sum(sophia)
-            ganancia_mateo = sum(mateo)
+    with open(file_path, "r") as file:
+        file.readline()
+        fila = list(int(i) for i in file.readline().split(";"))
+        sophia, mateo, esperados = problema_monedas(fila)
+        ganancia_sophia = sum(sophia)
+        ganancia_mateo = sum(mateo)
 
-            print(f"Test {test_files[i]}\nGanancia Sophia: {ganancia_sophia}")
-            print(f"Ganancia Mateo: {ganancia_mateo}")
-            if ganancia_sophia > ganancia_mateo:
-                print("Resultado: Sophia ganó.")
-            else:
-                print("Resultado: Sophia perdió.")
+        print(f"Archivo {file_path}\nGanancia Sophia: {ganancia_sophia}")
+        print(f"Ganancia Mateo: {ganancia_mateo}")
+        if ganancia_sophia > ganancia_mateo:
+            print("Resultado: Sophia ganó.")
+        else:
+            print("Resultado: Sophia perdió.")
 
-            print("")
+        print("")
 
-            with open(f"{output_folder}/resultado_catedra_test_{i+1}.txt", "w") as output_file:
-                output_file.write(f"Esperados: {'; '.join(esperados)}\n")
-    # Tests propios
-    test_folder = "tests_cases"
-    test_files_propios = [
-        "test1", "test2", "test3", "test4", "test5",
-        "test6", "test7", "test8", "test9", "test10"
-    ]
-    print("")
-    for i, test_file in enumerate(test_files_propios):
-        with open(f"{test_folder}/{test_file}.txt", "r") as file:
-            file.readline()
-            fila = list(int(i) for i in file.readline().split(";"))
-            sophia, mateo, esperados = problema_monedas(fila)
-            ganancia_sophia = sum(sophia)
-            ganancia_mateo = sum(mateo)
-
-            print(f"Test propio {i+1}\nGanancia Sophia: {ganancia_sophia}")
-            print(f"Ganancia Mateo: {ganancia_mateo}")
-            if ganancia_sophia > ganancia_mateo:
-                print("Resultado: Sophia ganó.")
-            else:
-                print("Resultado: Sophia perdió.")
-
-            print("")
-
-            with open(f"{output_folder}/resultado_test_propio{i+1}.txt", "w") as output_file:
-                output_file.write(f"Esperados: {'; '.join(esperados)}\n")
+        output_filename = os.path.basename(file_path).replace(".txt", "_resultado.txt")
+        with open(f"{output_folder}/{output_filename}", "w") as output_file:
+            output_file.write(f"Esperados: {'; '.join(esperados)}\n")
 
 if __name__ == "__main__":
-    tests()
+    if len(sys.argv) != 2:
+        print("Argumentos incorrectos.\nUso: python3 problema.py <archivo_set_de_datos.txt>")
+    else:
+        file_path = sys.argv[1]
+        tests(file_path)
